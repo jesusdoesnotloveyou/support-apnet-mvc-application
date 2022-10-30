d27d1290e8913ca440a7d277de986150d9740f9f
Artyom Kozlovsky <kozlovskyartem02@gmail.com>
2022-10-30 23:26:11 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SupportWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
